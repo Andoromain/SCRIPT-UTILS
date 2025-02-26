2ed31bcd58a1f8baeffcb1db3edc4b4f1d04e7c4
andoromain <andoajpr@gmail.com>
2025-02-26 17:01:55 +0300

    update script

--- createNewProjet.sh
+++ createNewProjet.sh
@@ -7,39 +7,100 @@ projectTypes=("Laravel" "React" "Vue")
 projectName=""
 projectUrl=""
 projectType=""
-projectPhpVersion=""
 projectDirectory=""
+projectPhpVersion=""
+projectEnvironment=""
 
-printWelcome(){
-	echo "******* Bienvenue sur l'assistance de nouveau projet ***********"
-	echo -e "\n\n"
+if [ $# -ge 1 ]; then
+  projectName="$1"
+fi
+if [ $# -ge 2 ]; then
+  projectUrl="$2"
+fi
+if [ $# -ge 3 ]; then
+  projectDirectory="$3"
+fi
+if [ $# -ge 4 ]; then
+  projectType="$4"
+fi
+if [ $# -ge 5 ]; then
+  projectPhpVersion="$5"
+fi
+if [ $# -ge 6 ]; then
+  projectEnvironment="$6"
+fi
+
+printWelcome() {
+  echo "******* Bienvenue sur l'assistance de nouveau projet ***********"
+  echo -e "\n\n"
 }
 
-printTab(){ 
+printTab() {
   index=1
   local table=("$@")
   for element in "${table[@]}"; do
-  	echo -e "$index - $element"
-  	((index++))
+    echo -e "$index - $element"
+    ((index++))
   done
 }
 
-readProjectStructure(){
-  read -p "Saisir le nom du projet  : " projectName
-  read -p "Saisir l'url du projet (sans http://) : " projectUrl
-  read -p "Saisir le chemin du projet : " projectDirectory
+readProjectStructure() {
+  if [ -z "$projectName" ]; then
+    read -p "Saisir le nom du projet  : " projectName
+  else
+    echo "Nom du projet (default): $projectName"
+  fi
 
-  echo -e "Choisir le type de projet"
+  if [ -z "$projectUrl" ]; then
+    read -p "Saisir l'url du projet (sans http://) : " projectUrl
+  else
+    echo "Url du projet (default): $projectUrl"
+  fi
+
+  if [ -z "$projectDirectory" ]; then
+    read -p "Saisir le chemin du projet : " projectDirectory
+  else
+    echo "Chemin du projet (default): $projectDirectory"
+  fi
+
+  echo -e "\nChoisir le type de projet"
   printTab "${projectTypes[@]}"
-  read -p "Choix : " projectType
-  
-  if [ "$projectType" = "Laravel" ]; then
+  if [ -z "$projectType" ]; then
+    read -p "Choix : " projectType
+  else
+    echo "Type du projet (default): $projectType"
+    if [ "$projectType" = "1" ]; then
+      projectType="Laravel"
+    elif [ "$projectType" = "2" ]; then
+      projectType="React"
+    elif [ "$projectType" = "3" ]; then
+      projectType="Vue"
+    fi
+  fi
+
+  if [ "$projectType" = "Laravel" ] && [ -z "$projectPhpVersion" ]; then
     read -p "Version du php : " projectPhpVersion
+  elif [ "$projectType" = "Laravel" ]; then
+    echo "Version du PHP (default): $projectPhpVersion"
+  fi
+
+  if [ -z "$projectEnvironment" ]; then
+    read -p "Environnement (dev, prod) : " projectEnvironment
+    if [ "$projectEnvironment" != "dev" ] && [ "$projectEnvironment" != "prod" ]; then
+      echo "Environnement non reconnu. Veuillez choisir entre dev ou prod."
+      read -p "Environnement (dev, prod) : " projectEnvironment
+    fi
+  else
+    echo "Environnement (default): $projectEnvironment"
+  fi
+
+   if [[ ( "$projectType" == "React" || "$projectType" == "Vue" ) && -z "$projectEnvironment" ]]; then 
+    # Your commands here
   fi
 }
 
-createVirtualUrl(){
-   echo "127.0.0.1 $projectUrl" | sudo tee -a /etc/hosts > /dev/null
+createVirtualUrl() {
+  echo "127.0.0.1 $projectUrl" | sudo tee -a /etc/hosts >/dev/null
 }
 
 createVirtualHostNginx() {
@@ -58,8 +119,8 @@ createVirtualHostNginx() {
   sudo touch "$configFile"
 
   case "$projectType" in
-    Laravel)
-      cat <<EOF | sudo tee "$configFile"
+  Laravel)
+    cat <<EOF | sudo tee "$configFile"
 server {
     listen 80;
     server_name $projectUrl;
@@ -83,10 +144,10 @@ server {
     }
 }
 EOF
-      ;;
-    
-    React|Vue)
-      cat <<EOF | sudo tee "$configFile"
+    ;;
+
+  React | Vue)
+    cat <<EOF | sudo tee "$configFile"
 server {
     listen 80;
     server_name $projectUrl;
@@ -99,12 +160,12 @@ server {
     }
 }
 EOF
-      ;;
-    
-    *)
-      echo "Type de projet non reconnu. Veuillez choisir entre Laravel, React ou Vue."
-      return 1
-      ;;
+    ;;
+
+  *)
+    echo "Type de projet non reconnu. Veuillez choisir entre Laravel, React ou Vue."
+    return 1
+    ;;
   esac
 
   # Activer le site et recharger Nginx
@@ -114,15 +175,11 @@ EOF
   echo "Virtual Host créé et activé pour $projectUrl ($projectType)"
 }
 
-
 ## fonction main :::
 
 printWelcome
 readProjectStructure
 # createVirtualHostNginx "$projectUrl" "$projectType" "$projectPhpVersion"
-# createVirtualUrl 
+# createVirtualUrl
 
 echo -e " ------- Terminé ------ "
-
-
-
